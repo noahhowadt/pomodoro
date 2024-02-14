fe824eaa2a9d82634e9e229e5e12a2a1fa4c965b
Noah Howadt <noah.howadt@gmail.com>
2024-02-14 19:16:06 +0100

    Add vercel + tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
-import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  adapter: vercel(),
+  integrations: [tailwind()],
+});
